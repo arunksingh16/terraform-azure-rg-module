9e692badda9131bfa6174d32498efb68aa1c0bad
Arun K Singh <22137632+arunksingh16@users.noreply.github.com>
2021-10-12 11:37:03 +0100

    Update input.tf

--- modules/rg/input.tf
+++ modules/rg/input.tf
@@ -1,1 +1,12 @@
+variable "azure_rg_name" {
+  description = "Azure Resource Group Name"
+}
 
+variable "location" {
+  description = "Default Location"
+}
+
+variable "tf-env" {
+  type    = string
+  default =  "poc"
+}
